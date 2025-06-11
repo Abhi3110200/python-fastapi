fadf10ba149c1ea2e4695ac4d7663db8b6664195
Abhi3110200 <abhijeetdrv@gmail.com>
2025-06-11 14:37:20 +0530

    init commit

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {'data': {'message': 'Hello, World!'}}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {'data': {'item_id': item_id, 'query': q}}
